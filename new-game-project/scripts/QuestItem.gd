b1fd94aa8af78f17e73eb082869704882de5d76b
fishygaminglife-del <fishygaming.life@gmail.com>
2025-12-29 21:33:35 -0600

    puzzle images

--- new-game-project/scripts/QuestItem.gd
+++ new-game-project/scripts/QuestItem.gd
@@ -1,8 +1,7 @@
 extends Area2D
 
 
-@export var item_id: String = ""
-@export var item_quantity: int = 1
+
 signal pressed
 
 
@@ -16,5 +15,9 @@ func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton \
 	and event.pressed \
 	and event.button_index == MOUSE_BUTTON_LEFT:
-		$Gamebox.visible=true
-		emit_signal("pressed")
+		counter +=1 
+		if counter == 1:
+			$Gamebox.visible=true
+			emit_signal("pressed")
+		if counter == 2:
+			get_tree().change_scene_to_file("res://scene/puzzle.tscn")
--- new-game-project/scripts/house.gd
+++ new-game-project/scripts/house.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
-
+var counter = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$QuestItem/CollisionShape2D.disabled = true
 	$QuestItem.pressed.connect(box_pressed)
 	$AnimationPlayer/TextBox.visible = true
 	$AnimationPlayer.play("text")
@@ -11,14 +12,19 @@ func _ready() -> void:
 	await $AnimationPlayer.animation_finished
 	$AnimationPlayer.play("text2")
 	await get_tree().create_timer(5).timeout
+	$QuestItem/CollisionShape2D.disabled = false
 	$AnimationPlayer/TextBox.visible = false
 	await $AnimationPlayer.animation_finished
+	counter = 1
+	
 func box_pressed():
+	$QuestItem/CollisionShape2D.disabled = true	
 	$AnimationPlayer/TextBox.visible = true
 	$AnimationPlayer.play("GameBoard")
 	await get_tree().create_timer(5).timeout
 	$AnimationPlayer/TextBox.visible = false
 	await $AnimationPlayer.animation_finished
+	$QuestItem/CollisionShape2D.disabled = false
 	
 func game_pressed():
 	pass
