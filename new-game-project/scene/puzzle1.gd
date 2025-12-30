ff4bde40d0f1732a36db7900e826805b9795fc7a
fishygaminglife-del <fishygaming.life@gmail.com>
2025-12-30 11:14:29 -0600

    Puzzle Finished and works

--- new-game-project/scene/puzzle1.gd
+++ new-game-project/scene/puzzle1.gd
@@ -1,1 +1,63 @@
 extends Node2D
+
+@onready var timer_label = $TimerLabel
+
+
+var time_left = 30
+var timer_running = true
+
+var matched_pairs = 0
+var total_pairs = 6 
+
+var first_card = null
+var second_card = null
+
+func _ready() -> void:
+	$AnimationPlayer/TextBox.visible = true
+	$AnimationPlayer.play("MemoryGame")
+	await get_tree().create_timer(5).timeout
+	await $AnimationPlayer.animation_finished
+	$AnimationPlayer/TextBox.visible = false
+
+func _process(delta):
+	if timer_running:
+		time_left -= delta
+		if time_left == 0:
+			timer_running = false
+			time_left = 0
+			time_up()
+	timer_label.text = str(int(time_left))
+
+
+func card_flipped(card):
+	if first_card == null:
+		first_card = card
+	elif second_card == null:
+		second_card = card
+		check_match()
+		
+func check_match():
+	if first_card.card_type == second_card.card_type:
+		matched_pairs += 1
+		first_card = null
+		second_card = null
+		
+		if matched_pairs == total_pairs:
+			game_complete()
+			
+			
+	else:
+		await get_tree().create_timer(1.0).timeout
+		first_card.hide_front()
+		second_card.hide_front()
+		first_card = null
+		second_card = null
+	
+func game_complete():
+	await get_tree().create_timer(3).timeout
+	get_tree().change_scene_to_file("res://scene/house.tscn")
+	
+func time_up():
+	print("Time is Up!")
+	time_left = 0
+	get_tree().reload_current_scene()
--- new-game-project/scripts/card.gd
+++ new-game-project/scripts/card.gd
@@ -1,5 +1,5 @@
 extends Area2D
-
+@export var card_type: String
 var is_flipped = false
 
 
@@ -9,9 +9,14 @@ func _input_event(viewport, event, shape_idx):
 	and event.button_index == MOUSE_BUTTON_LEFT:
 		flip_card()
 
-func flip_card():
+func flip_card ():
+	if is_flipped:
+		return
+	
 	is_flipped = true
 	$Sprite2D2.modulate.a = 1.0
+
+	get_parent().card_flipped(self)
 
 func hide_front():
 	is_flipped = false
